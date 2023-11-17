1e36e63f8e87e8faa5f31e9a10d91ca1b5701a0d
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-17 14:06:14 +0900

    axios 세팅

---
+++ src/axiosSetting.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://localhost:5000";
+
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+Vue.prototype.$axios = axios;
