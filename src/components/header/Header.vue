e5d071d5485c37e18ad2670ef6e27d7846cd0f37
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-21 16:23:00 +0900

    Header 색깔 및 스타일 변경

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,16 +1,9 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  <div class="header">
+    <b-navbar type="dark" variant="none">
+      <b-navbar-brand href="/">Daily_Writing</b-navbar-brand>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
@@ -23,22 +16,6 @@
               >Search</b-button
             >
           </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -51,3 +28,7 @@ export default {
   compatConfig: { MODE: 3 },
 };
 </script>
+
+<style>
+@import "../../css/Header.css";
+</style>
