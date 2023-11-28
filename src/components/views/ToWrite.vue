2d5a1ea50e9651367dba99d99409445d60f8e69f
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-11-28 23:18:14 +0900

    글쓰기 기능 input 및 textarea 추가

--- src/components/views/ToWrite.vue
+++ src/components/views/ToWrite.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>hi</div>
+  <div>
+    <b-col cols="6">
+      <input class="writeTitle" placeholder="제목을 입력해주세요" />
+      <div>
+        <input class="writeContext" placeholder="내용" />
+      </div>
+    </b-col>
+  </div>
 </template>
 
 <script>
 export default {};
+
+import "../../css/Write.css";
 </script>
