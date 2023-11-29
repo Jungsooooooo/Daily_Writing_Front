225b975dc9263814f7876f5368429ed7658591a0
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-11-29 23:57:02 +0900

    footer 추가 후 버튼 css 추가

--- src/components/views/ToWrite.vue
+++ src/components/views/ToWrite.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="write">
+  <div>
     <b-col cols="6">
       <input class="writeTitle" placeholder="제목을 입력해주세요" />
       <div>
         <textarea class="writeContext" wrap="soft" placeholder="내용" />
       </div>
-      <template #footer> <div>hi</div> </template>
+      <footer class="saveWriteCol">
+        <b-button size="sm" class="saveWrite" @click="goToWrite()">저장</b-button>
+      </footer>
     </b-col>
   </div>
 </template>
