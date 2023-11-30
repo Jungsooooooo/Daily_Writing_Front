12fe3e448a327b12c192c25691f9775028a1d3e4
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-11-30 15:41:16 +0900

    axios 글쓰기 기능 구현

--- src/components/views/ToWrite.vue
+++ src/components/views/ToWrite.vue
@@ -6,14 +6,27 @@
         <textarea class="writeContext" wrap="soft" placeholder="내용" />
       </div>
       <footer class="saveWriteCol">
-        <b-button size="sm" class="saveWrite" @click="goToWrite()">저장</b-button>
+        <b-button size="sm" class="saveWrite" @click="saveWrite()">저장</b-button>
       </footer>
     </b-col>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  methods: {
+    saveWrite() {
+      let title = document.getElementsByClassName("writeTitle")[0].value;
+      let context = document.getElementsByClassName("writeContext")[0].value;
+      const input = {
+        title: title,
+        context: context,
+      };
+      axios.post("/api/writings/create", input);
+    },
+  },
+};
 
 import "../../css/Write.css";
 </script>
