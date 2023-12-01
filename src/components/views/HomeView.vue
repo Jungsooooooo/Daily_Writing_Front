dd03356ec537bdf156bac02e58d7526a55d865a1
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-12-01 13:53:56 +0900

    homeview css 및 card 추가

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
-    <b-card
-      title="Card Title"
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem"
-      class="mb-2"
-    >
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </b-card-text>
-    </b-card>
-  </div>
+  <b-col cols="12">
+    <div class="homeCard">
+      <b-card
+        v-for="item in data"
+        :key="item.title"
+        :title="item.title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2"
+      >
+        {{ item.context }}
+      </b-card>
+    </div>
+  </b-col>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import "../../css/HomeView.css";
+export default {
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted() {
+    axios.get("/api/writings").then((res) => {
+      this.data = res.data;
+      console.log(this.data);
+    });
+  },
+};
 </script>
