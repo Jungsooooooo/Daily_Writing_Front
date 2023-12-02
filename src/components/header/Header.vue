135d783a27631dfa9d19e2f8a249a7ae5c0253fe
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-02 21:22:48 +0900

    찾기 버튼 및 input 대신 search icon 추가

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -7,9 +7,8 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" id="search" @click="click()">Search</b-button>
-            <b-button size="sm" class="writeButton" id="search" @click="goToWrite()">새 글 쓰기</b-button>
+            <b-icon-search class="searchIcon" />
+            <b-button size="sm" class="writeButton" @click="goToWrite()">새 글 쓰기</b-button>
           </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
