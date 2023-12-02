52219385ab32c1973fee9681d2c45f3a67c8dcc3
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-02 20:50:15 +0900

    기본 페이지에 글 목록 보이게 하기

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ const router = new VueRouter({
   mode: "history",
   routes: [
     {
-      path: "/home",
+      path: "/",
       component: HomeView,
     },
     { path: "/write", component: ToWrite },
