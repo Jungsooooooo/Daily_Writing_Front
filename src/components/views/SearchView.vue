f96f35bd0c4ebdabf5d2e8484f818ce1845bb93b
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-06 23:51:16 +0900

    검색화면 css 수정 및 placeholder 수정

--- src/components/views/SearchView.vue
+++ src/components/views/SearchView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="searchBar">
     <b-row>
       <b-col cols="4"></b-col>
       <b-col cols="4" align-self="center">
         <b-input-group class="mt3">
-          <b-form-input></b-form-input>
+          <b-form-input placeholder="검색어를 입력해주세요"></b-form-input>
           <b-button>Search</b-button>
         </b-input-group>
       </b-col>
@@ -13,5 +13,7 @@
 </template>
 
 <script>
+import "../../css/Search.css";
+
 export default {};
 </script>
