55c2ff94949c751bccfdb293f317fa01acde8e8f
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-11 23:24:08 +0900

    중앙 정렬 적용

--- src/components/views/DetailView.vue
+++ src/components/views/DetailView.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class="detailView">
     {{ $route.query.title }}
     {{ $route.query.context }}
   </div>
 </template>
 
 <script>
+import "../../css/DetailView.css";
 export default {};
 </script>
