4741625db4797061b74721300b5bd311da057b13
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-14 23:39:35 +0900

    삭제 기능 추가

--- src/components/views/DetailView.vue
+++ src/components/views/DetailView.vue
@@ -7,10 +7,31 @@
     <div class="detailContext">
       <p>{{ $route.query.context }}</p>
     </div>
+
+    <div>
+      <b-link @click="deleteWriting($route.query.id)">삭제</b-link>
+    </div>
   </div>
 </template>
 
 <script>
 import "../../css/DetailView.css";
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      longId: this.id,
+    };
+  },
+  computed: {},
+
+  methods: {
+    deleteWriting() {
+      axios.delete("/api/writings/delete/" + this.longId).then(() => {
+        console.log("삭제완료");
+      });
+    },
+  },
+};
 </script>
