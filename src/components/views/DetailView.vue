297b3cc6b4d6f9bf0b09e72fb6119998b6bef2d0
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-12-23 22:44:35 +0900

    상세페이지 수정 버튼 및 위치 변경

--- src/components/views/DetailView.vue
+++ src/components/views/DetailView.vue
@@ -4,12 +4,13 @@
       <h1 class="detailTitle">{{ $route.query.title }}</h1>
     </div>
 
-    <div class="detailContext">
-      <p>{{ $route.query.context }}</p>
-    </div>
-
     <div>
       <b-link @click="deleteWriting($route.query.id)">삭제</b-link>
+      <b-link class="updateWriting" @click="updateWriting($route.query.id)">수정</b-link>
+    </div>
+
+    <div class="detailContext">
+      <p>{{ $route.query.context }}</p>
     </div>
   </div>
 </template>
@@ -30,6 +31,12 @@ export default {
     deleteWriting(id) {
       axios.delete("/api/writings/delete/" + id).then(() => {
         console.log("삭제완료");
+      });
+    },
+    updateWriting() {
+      this.$router.push({
+        path: "/update",
+        query: { title: this.title, context: this.context, id: this.id },
       });
     },
   },
