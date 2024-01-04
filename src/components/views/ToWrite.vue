f867f9b29601a3dc4ea5a0485963c50d312aa2ab
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-01-04 16:39:11 +0900

    이미지 드래그 드랍 기능 시 파일 이름 textarea에 들어오게 끔 추가

--- src/components/views/ToWrite.vue
+++ src/components/views/ToWrite.vue
@@ -1,23 +1,52 @@
 <template>
   <div>
-    <b-col cols="6">
-      <input class="writeTitle" placeholder="제목을 입력해주세요" />
-      <div>
-        <textarea class="writeContext" wrap="soft" placeholder="내용" />
-      </div>
-      <footer class="saveWriteCol">
-        <b-button size="sm" class="saveWrite" @click="saveWrite()">저장</b-button>
-      </footer>
-    </b-col>
-    <div>
-      <b-modal ref="saveModal"></b-modal>
-    </div>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="6">
+          <input class="writeTitle" placeholder="제목을 입력해주세요" />
+
+          <div>
+            <textarea
+              class="writeContext"
+              wrap="soft"
+              placeholder="내용"
+              @dragover.prevent
+              @drop="handleDrop"
+              v-model="imageData"
+              @input="contextInput"
+            />
+          </div>
+          <footer class="saveWriteCol">
+            <b-button size="sm" class="saveWrite" @click="saveWrite()"
+              >저장</b-button
+            >
+          </footer>
+        </b-col>
+
+        <b-col cols="6">
+          <textarea
+            v-model="imageData"
+            class="writeContext"
+            readonly
+          ></textarea>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal ref="saveModal"></b-modal>
   </div>
 </template>
 
+<script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <script>
 import axios from "axios";
 export default {
+  data() {
+    return {
+      image: null,
+      imageData: "",
+      contextInput: "",
+    };
+  },
   methods: {
     saveWrite() {
       let title = document.getElementsByClassName("writeTitle")[0].value;
@@ -28,11 +57,32 @@ export default {
       };
       axios.post("/api/writings/create", input).then((res) => {
         console.log(res.data);
-        this.$bvModal.msgBoxOk(res.data.title + "이 작성되었습니다.").then(() => {
-          this.$router.push("/");
-        });
+        this.$bvModal
+          .msgBoxOk(res.data.title + "이 작성되었습니다.")
+          .then(() => {
+            this.$router.push("/");
+          });
       });
     },
+    handleDrop(event) {
+      event.preventDefault();
+
+      // 드롭된 파일 가져오기
+      const file = event.dataTransfer.files[0];
+      console.log({ file });
+      // 파일이 이미지인지 확인
+      if (file && file.type.startsWith("image/")) {
+        // 이미지 파일이면 데이터에 저장
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.image = reader.result;
+          this.imageData += "\n" + file.name;
+        };
+        reader.readAsText(file);
+      }
+    },
+
+    contextInput() {},
   },
 };
 
