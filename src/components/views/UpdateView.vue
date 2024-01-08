ecccc0ec22596539e0fa009b616dc5bcdbda6a86
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-01-08 09:57:06 +0900

    수정 view 드래그 앤 드랍 기능 추가

--- src/components/views/UpdateView.vue
+++ src/components/views/UpdateView.vue
@@ -3,10 +3,19 @@
     <b-col cols="6">
       <input v-model="titleValue" class="writeTitle" />
       <div>
-        <textarea v-model="contextValue" class="writeContext" wrap="soft" />
+        <textarea
+          v-model="contextValue"
+          class="writeContext"
+          wrap="soft"
+          @dragover.prevent
+          @drop="handleDrop"
+          @input="contextInput"
+        />
       </div>
       <footer class="saveWriteCol">
-        <b-button size="sm" class="saveWrite" @click="updateWrite()">수정</b-button>
+        <b-button size="sm" class="saveWrite" @click="updateWrite()"
+          >수정</b-button
+        >
       </footer>
     </b-col>
     <div>
@@ -49,10 +58,29 @@ export default {
       };
       axios.post("/api/writings/update", input).then((res) => {
         console.log(res.data);
-        this.$bvModal.msgBoxOk(res.data.title + "이 수정되었습니다.").then(() => {
-          this.$router.push("/");
-        });
+        this.$bvModal
+          .msgBoxOk(res.data.title + "이 수정되었습니다.")
+          .then(() => {
+            this.$router.push("/");
+          });
       });
+    },
+    handleDrop(event) {
+      event.preventDefault();
+
+      // 드롭된 파일 가져오기
+      const file = event.dataTransfer.files[0];
+      console.log({ file });
+      // 파일이 이미지인지 확인
+      if (file && file.type.startsWith("image/")) {
+        // 이미지 파일이면 데이터에 저장
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.image = reader.result;
+          this.contextValue += "\n" + file.name;
+        };
+        reader.readAsText(file);
+      }
     },
   },
 };
