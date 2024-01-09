c3648816af244dac4d32bb99f812ee2452ad9d6a
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2024-01-09 17:00:27 +0900

    사진 첨부 기능 시 ')'로 끝나지 않으면 문자로 입력 값 받을 수 있게 설정

--- src/components/views/ToWrite.vue
+++ src/components/views/ToWrite.vue
@@ -3,7 +3,11 @@
     <b-container fluid>
       <b-row>
         <b-col cols="6">
-          <input class="writeTitle" placeholder="제목을 입력해주세요" />
+          <input
+            class="writeTitle"
+            placeholder="제목을 입력해주세요"
+            @input="updateTitlePreview"
+          />
 
           <div>
             <textarea
@@ -13,7 +17,6 @@
               @dragover.prevent
               @drop="handleDrop"
               v-model="imageData"
-              @input="contextInput"
             />
           </div>
           <footer class="saveWriteCol">
@@ -24,11 +27,26 @@
         </b-col>
 
         <b-col cols="6">
-          <textarea
-            v-model="imageData"
-            class="writeContext"
-            readonly
-          ></textarea>
+          <h1><input type="text" v-model="title" class="preShowTitle" /></h1>
+          <div v-for="(line, index) in imageData.split('\n')" :key="index">
+            <template v-if="line.startsWith('![]') && line.endsWith(')')">
+              <input
+                class="preShowImageWriting"
+                type="image"
+                :src="extractImageUrl(line)"
+              />
+            </template>
+            <template v-else>
+              <textarea
+                v-model="lines[index]"
+                class="preShowTextWriting"
+                @input="adjustTextareaHeight"
+                ref="dynamicTextarea"
+                :style="{ height: dynamicHeight }"
+                readonly
+              />
+            </template>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -39,18 +57,32 @@
 <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
       image: "",
       imageData: "",
       contextInput: "",
+      inputContainer: document.getElementById("inputContainer"),
+      lines: [],
+      title: "",
+      titlePreview: "",
+      dynamicHeight: "auto",
     };
+  },
+  watch: {
+    imageData: function (newText) {
+      // textarea의 각 줄을 배열로 분리
+
+      this.lines = newText.split("\n");
+    },
   },
   methods: {
     saveWrite() {
       let title = document.getElementsByClassName("writeTitle")[0].value;
       let context = document.getElementsByClassName("writeContext")[0].value;
+
       const input = {
         title: title,
         context: context,
@@ -78,7 +110,13 @@ export default {
 
         reader.onload = (e) => {
           this.image = e.target.result.split(",")[1];
-          this.imageData += "\n" + "![]" + "(" + file.name + ")";
+          this.imageData +=
+            "\n" +
+            "![]" +
+            "(" +
+            "http://192.168.67.128/images/" +
+            file.name +
+            ")";
         };
 
         reader.readAsDataURL(file);
@@ -93,11 +131,21 @@ export default {
         });
       }, 100);
     },
-    // readfile(file){
-
-    // }
-
-    contextInput() {},
+    extractImageUrl(line) {
+      var match = line.match(/\((.*?)\)/);
+      if (match && match[1]) {
+        return match[1].trim();
+      } else {
+        return "";
+      }
+    },
+    updateTitlePreview() {
+      this.title = document.getElementsByClassName("writeTitle")[0].value;
+    },
+    adjustTextareaHeight() {
+      // 입력된 텍스트의 높이에 따라 동적으로 늘어나도록 높이 조절
+      this.dynamicHeight = this.$refs.dynamicTextarea.scrollHeight + "px";
+    },
   },
 };
 
